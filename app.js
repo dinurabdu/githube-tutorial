91b090253bb2c934a56c359eb1fb249028f35235
DiNuR <dinurabdu@gmail.com>
2022-10-16 21:55:44 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// const loveColors = [];
+// loveColors[0] = prompt("What's your love color #1", "");
+// loveColors[1] = prompt("What's your love color #2", "");
+// loveColors[2] = prompt("What's your love color #3", "");
+// console.log(loveColors);
+// const channel = prompt("What's your youtube");
+// console.log(`https://youtube.com/${channel}/videos`);
+
+// console.log(6 + +"9");
+
+// let incr = 10;
+// let decr = 10;
+// incr++;
+// decr--;
+// console.log(incr++);
+// console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
+// console.log(incr);
+// console.log(decr);
+
+// console.log(3 * 3 == 9);
+// console.log(3 * 3 === "9");
+
+const isAge = true;
+const isClose = false;
+const isChecked = true;
+
+console.log(isAge && isClose);
+console.log(isAge || isClose);
+console.log(isAge && !isClose && isChecked);
